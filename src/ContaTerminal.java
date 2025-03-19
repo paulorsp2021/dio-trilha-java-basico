9a50c8f4119d6c7e083f8cb06e9ea5c14733cdae
paulorsp2021 <paulorsp2021@gmail.com>
2025-03-19 17:14:48 -0300

    entrega

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int agencia = scanner.nextInt();
+        System.out.println("Por favor, digite a sua conta !");
+        String numero = scanner.next();
+        System.out.println("Por favor, digite seu nome !");
+        String nome = scanner.next();
+        System.out.println("Por favor, digite seu saldo !");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco," +
+                " sua agência é " + agencia
+                + ", conta " + numero +
+                " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
